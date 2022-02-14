16c432d1f00869e969c586687935dcb117260ba9
Andy Herron <coachfozzie@gmail.com>
2022-02-14 00:54:34 -0500

    working on the challenge

--- charts.js
+++ charts.js
@@ -1,11 +1,10 @@
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
+    console.log(data);
     var sampleNames = data.names;
-
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -15,22 +14,23 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
-    buildCharts(firstSample);
+    // buildCharts(firstSample);
     buildMetadata(firstSample);
   });
-}
+};
 
 // Initialize the dashboard
 init();
 
+// create the function for selecting the new person's data
+// this function is called from the HTML file
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
   buildMetadata(newSample);
-  buildCharts(newSample);
-  
-}
+  // buildCharts(newSample);
+};
 
 // Demographics Panel 
+// this function fills in the person's demographic info, pulled from the metadata array.
 function buildMetadata(sample) {
   d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
@@ -39,10 +39,8 @@ function buildMetadata(sample) {
     var result = resultArray[0];
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
-
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
-
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
@@ -58,15 +56,16 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+      var sampleValues = data.samples
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+      var filteredSamples = sampleValues.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
-
+      var oneSample = filteredSamples[0];
+      console.log(oneSample);
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
-
+      var my_otu_ids = oneSample.otu_ids
+      var my_otu_labels = oneSample.my_otu_labels
+      var my_sample_values = oneSample.sample_values
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
@@ -74,14 +73,14 @@ function buildCharts(sample) {
     var yticks = 
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    // var barData = [
+
+   // ];
     // 9. Create the layout for the bar chart. 
-    var barLayout = {
+   // var barLayout = {
      
-    };
+   // };
     // 10. Use Plotly to plot the data with the layout. 
     
   });
-}
+};
